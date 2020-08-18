4438a381d59697c3c9ad7fb5e7b7090cb9c03281
DTOVAR_PRUEBAS1 <rtovarsystem@hotmail.com>
2020-08-18 11:46:40 -0500

    Primer Commit Ejemplo

---
+++ AplicacionWeb1/Scripts/otro.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+        
+       $('#submenu li a').on('click', function(e){
+          // $('#submenu').removeClass('in');
+         $('#submenu').toggleClass();
+       });
+       
+    });
